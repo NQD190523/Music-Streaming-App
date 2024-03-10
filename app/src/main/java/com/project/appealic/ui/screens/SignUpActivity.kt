b8974442e5c21ae7612211e6dc235005fc608ccc
NQD190523 <duc19052003@gmail.com>
2024-03-10 16:58:30 +0700

    add feature

---
+++ app/src/main/java/com/project/appealic/ui/screens/SignUpActivity.kt
@@ -0,0 +1,20 @@
+package com.project.appealic.ui.screens
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.firebase.auth.FirebaseAuth
+import com.project.appealic.databinding.ActivityRegisterBinding
+
+class SignUpActivity: AppCompatActivity() {
+
+    private lateinit var binding: ActivityRegisterBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+    }
+
+}
