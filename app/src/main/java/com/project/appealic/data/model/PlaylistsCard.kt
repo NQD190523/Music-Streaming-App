3332840fa871445bd165d726e8b945423f136dca
thanhloc81 <locldt21411@st.uel.edu.vn>
2024-04-08 14:19:05 +0700

    add adapter lib

---
+++ app/src/main/java/com/project/appealic/data/model/PlaylistsCard.kt
@@ -0,0 +1,6 @@
+package com.project.appealic.data.model
+
+data class PlaylistsCard (
+    val namePlaylist: String,
+    val imagePlaylist: String? = null
+)
--- app/src/main/java/com/project/appealic/ui/view/Fragment/LibraryFragment.kt
+++ app/src/main/java/com/project/appealic/ui/view/Fragment/LibraryFragment.kt
@@ -1,15 +1,15 @@
-package com.project.appealic.ui.view.Fragment
-
+import android.graphics.Rect
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
 import com.project.appealic.R
 import com.project.appealic.ui.view.Adapters.BannerAdapter
+import com.project.appealic.ui.view.Adapters.PlaylistCardAdapter
 
 class LibraryFragment : Fragment() {
 
@@ -34,36 +34,43 @@ class LibraryFragment : Fragment() {
             LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
         recyclerViewBanner.adapter = bannerAdapter
 
-// Tìm kiếm các TextView tương ứng
-        val tvAddArtists = view.findViewById<TextView>(R.id.tvAddArtists)
-        val tvAddAlbums = view.findViewById<TextView>(R.id.tvAddAlbums)
-        val tvAddSongs = view.findViewById<TextView>(R.id.tvAddPlaylists)
+        // Danh sách hình ảnh từ thư mục drawable
+        val imageList = listOf(
+            R.drawable.libraryliked_song,
+            R.drawable.librarydownload,
+            R.drawable.playlist1,
+            R.drawable.playlistkpop,
+            R.drawable.playlistvpop,
+            R.drawable.playlisttiktok,
+            R.drawable.playlistgenz,
+            R.drawable.playlistlofi
+        )
 
-        // Thiết lập onClickListener cho TextViews
-        tvAddSongs.setOnClickListener {
-            // Thay thế fragment hiện tại bằng AddSongToPlaylistFragment
-            requireActivity().supportFragmentManager.beginTransaction()
-                .replace(R.id.fragmentAddPlaylistLibrary, AddPlaylistLibraryFragment())
-                .addToBackStack(null)
-                .commit()
-        }
+        // Tạo adapter và thiết lập cho card_recycle
+        val cardAdapter = PlaylistCardAdapter(requireContext(), imageList)
+        val recycleCardPlaylist : RecyclerView  = view.findViewById(R.id.card_gird)
 
-        tvAddArtists.setOnClickListener {
-            // Replace the current fragment with AddArtistFragment
-            requireActivity().supportFragmentManager.beginTransaction()
-                .replace(R.id.fragmentAddPlaylistLibrary, AddArtistFragment())
-                .addToBackStack(null)
-                .commit()
-        }
+        recycleCardPlaylist.layoutManager = StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.HORIZONTAL)
 
-        tvAddAlbums.setOnClickListener {
-            // Replace the current fragment with AddAlbumFragment
-            requireActivity().supportFragmentManager.beginTransaction()
-                .replace(R.id.fragmentAddPlaylistLibrary, AddAlbumFragment())
-                .addToBackStack(null)
-                .commit()
-        }
+        recycleCardPlaylist.adapter = cardAdapter
+
+        val horizontalSpacing = resources.getDimensionPixelSize(R.dimen.dp_2) // Khoảng cách ngang mong muốn
+        val verticalSpacing = resources.getDimensionPixelSize(R.dimen.dp_2) // Khoảng cách dọc mong muốn
+        val decoration = StaggeredGridSpacingItemDecoration(horizontalSpacing, verticalSpacing)
+        recycleCardPlaylist.addItemDecoration(decoration)
 
         return view
     }
+
+    class StaggeredGridSpacingItemDecoration(private val horizontalSpacing: Int, private val verticalSpacing: Int) : RecyclerView.ItemDecoration() {
+        override fun getItemOffsets(outRect: Rect, view: View, parent: RecyclerView, state: RecyclerView.State) {
+            outRect.apply {
+                left = horizontalSpacing / 6
+                right = horizontalSpacing / 6
+                bottom = verticalSpacing
+            }
+        }
+    }
+
+
 }
