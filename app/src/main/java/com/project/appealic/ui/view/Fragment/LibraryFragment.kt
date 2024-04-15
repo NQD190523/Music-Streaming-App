4474e9f1716e78fd9521e5e93c2b03e27308cf69
Lê Thị Băng Duyên <duyenltb21411@st.uel.edu.vn>
2024-04-16 00:16:07 +0700

    transation fragment

--- app/src/main/java/com/project/appealic/ui/view/Fragment/LibraryFragment.kt
+++ app/src/main/java/com/project/appealic/ui/view/Fragment/LibraryFragment.kt
@@ -10,21 +10,30 @@ import android.view.animation.AnimationSet
 import android.view.animation.TranslateAnimation
 import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.StaggeredGridLayoutManager
+import com.google.firebase.auth.FirebaseAuth
 import com.project.appealic.R
+import com.project.appealic.data.repository.SongRepository
+import com.project.appealic.data.repository.UserRepository
 import com.project.appealic.ui.view.Adapters.BannerAdapter
 import com.project.appealic.ui.view.Adapters.PlaylistCardAdapter
+import com.project.appealic.ui.view.Adapters.RecentlySongAdapter
 import com.project.appealic.ui.view.Fragment.AddAlbumFragment
 import com.project.appealic.ui.view.Fragment.AddArtistFragment
 import com.project.appealic.ui.view.Fragment.AddPlaylistLibraryFragment
+import com.project.appealic.ui.viewmodel.SongViewModel
+import com.project.appealic.utils.SongViewModelFactory
 
 class LibraryFragment : Fragment() {
 
+    private lateinit var songViewModel: SongViewModel
+
     @SuppressLint("MissingInflatedId")
     override fun onCreateView(
-
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -32,13 +41,14 @@ class LibraryFragment : Fragment() {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_library, container, false)
 
-
         val bannerImages = listOf(
             R.drawable.imagecard2,
             R.drawable.imagecard2,
             R.drawable.imagecard2
         )
         val bannerAdapter = BannerAdapter(bannerImages)
+        val factory = SongViewModelFactory(SongRepository(requireActivity().application), UserRepository(requireActivity().application))
+        songViewModel = ViewModelProvider(this, factory).get(SongViewModel::class.java)
 
 
         // Initialize and configure the RecyclerView for banner
@@ -52,43 +62,51 @@ class LibraryFragment : Fragment() {
         val tvAddAlbums = view.findViewById<TextView>(R.id.tvAddAlbums)
         val tvAddSongs = view.findViewById<TextView>(R.id.tvAddPlaylists)
 
-//        val underline = view.findViewById<View>(R.id.underline)
+        val underline = view.findViewById<View>(R.id.underline)
+
+//recently view song
+        val recentlyViewSong: RecyclerView = view.findViewById(R.id.recyclerView)
+        recentlyViewSong.layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
+        val recentlySongAdapter = RecentlySongAdapter(requireContext(), emptyList())
+        recentlyViewSong.adapter = recentlySongAdapter
+        songViewModel.getRecentSongs(FirebaseAuth.getInstance().currentUser?.uid.toString()).observe(viewLifecycleOwner, Observer { songs ->
+            recentlySongAdapter.updateData(songs)
+        })
+
 
 // Lưu trữ vị trí ban đầu của underline
         val initialX = 203f
-
-// Tính toán deltaX dựa trên vị trí của từng TextView
         val deltaXSongs =  tvAddSongs.x - initialX*2
         val deltaXArtists = tvAddArtists.x - initialX*5/4
         val deltaXAlbums = tvAddAlbums.x - initialX * 1/2
 
-//// Thiết lập onClickListener cho TextViews
-//        tvAddSongs.setOnClickListener {
-//            // Thay thế fragment hiện tại bằng AddSongToPlaylistFragment
-//            requireActivity().supportFragmentManager.beginTransaction()
-//                .replace(R.id.fragmentAddPlaylistLibrary, AddPlaylistLibraryFragment())
-//                .addToBackStack(null)
-//                .commit()
-//            underline.animate().translationX(deltaXSongs).setDuration(300).start()
-//        }
-//
-//        tvAddArtists.setOnClickListener {
-//            // Replace the current fragment with AddArtistFragment
-//            requireActivity().supportFragmentManager.beginTransaction()
-//                .replace(R.id.fragmentAddPlaylistLibrary, AddArtistFragment())
-//                .addToBackStack(null)
-//                .commit()
-//            underline.animate().translationX(deltaXArtists).setDuration(300).start()
-//        }
-//
-//        tvAddAlbums.setOnClickListener {
-//            // Replace the current fragment with AddAlbumFragment
-//            requireActivity().supportFragmentManager.beginTransaction()
-//                .replace(R.id.fragmentAddPlaylistLibrary, AddAlbumFragment())
-//                .addToBackStack(null)
-//                .commit()
-//            underline.animate().translationX(deltaXAlbums).setDuration(300).start()
-//        }
+// Thiết lập onClickListener cho TextViews
+        tvAddSongs.setOnClickListener {
+            // Thay thế fragment hiện tại bằng AddSongToPlaylistFragment
+            requireActivity().supportFragmentManager.beginTransaction()
+                .replace(R.id.fragmentAddPlaylistLibrary, AddPlaylistLibraryFragment())
+                .addToBackStack(null)
+                .commit()
+            underline.animate().translationX(deltaXSongs).setDuration(300).start()
+        }
+
+        tvAddArtists.setOnClickListener {
+            // Replace the current fragment with AddArtistFragment
+            requireActivity().supportFragmentManager.beginTransaction()
+                .replace(R.id.fragmentAddPlaylistLibrary, AddArtistFragment())
+                .addToBackStack(null)
+                .commit()
+            underline.animate().translationX(deltaXArtists).setDuration(300).start()
+        }
+
+        tvAddAlbums.setOnClickListener {
+            // Replace the current fragment with AddAlbumFragment
+            requireActivity().supportFragmentManager.beginTransaction()
+                .replace(R.id.fragmentAddPlaylistLibrary, AddAlbumFragment())
+                .addToBackStack(null)
+                .commit()
+            underline.animate().translationX(deltaXAlbums).setDuration(300).start()
+        }
 
         // Danh sách hình ảnh từ thư mục drawable
         val imageList = listOf(
