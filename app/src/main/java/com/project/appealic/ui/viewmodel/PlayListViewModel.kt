12ee769a8043e5c3f47b51633585313523356210
NQD190523 <duc19052003@gmail.com>
2024-04-17 00:01:11 +0700

    save working on playlist

--- app/src/main/java/com/project/appealic/ui/viewmodel/PlayListViewModel.kt
+++ app/src/main/java/com/project/appealic/ui/viewmodel/PlayListViewModel.kt
@@ -9,6 +9,7 @@ import androidx.lifecycle.viewModelScope
 import com.project.appealic.R
 import com.project.appealic.data.model.Artist
 import com.project.appealic.data.model.PlayListEntity
+import com.project.appealic.data.model.Playlist
 import com.project.appealic.data.model.UserPlaylist
 import com.project.appealic.data.model.UserWithPlayLists
 import com.project.appealic.data.repository.PlayListRepository
@@ -20,6 +21,9 @@ import kotlin.random.Random
 class PlayListViewModel(private val playListRepository: PlayListRepository) :ViewModel() {
     private val _userPlayLists = MutableLiveData<List<PlayListEntity>>()
     val userPlayLists: LiveData<List<PlayListEntity>> get() = _userPlayLists
+
+    private val _playLists = MutableLiveData<List<Playlist>>()
+    val playLists: LiveData<List<Playlist>> get() = _playLists
     fun createNewPlayList(playList: PlayListEntity) = viewModelScope.launch {
         withContext(Dispatchers.IO){
             playListRepository.createNewPlayList(playList)
@@ -30,5 +34,10 @@ class PlayListViewModel(private val playListRepository: PlayListRepository) :Vie
             playListRepository.getAllUserPlayList(uid)
         }
         _userPlayLists.postValue(playLists)
+    }
+    fun getAllPlayList(){
+        playListRepository.getAllTrack()
+            .addOnCompleteListener { playlist ->
+        }
     }
 }
