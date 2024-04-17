d4929107e6f72d917ceec278a5c64ce13cf42250
Lê Thị Băng Duyên <duyenltb21411@st.uel.edu.vn>
2024-04-17 14:38:38 +0700

    playlist

--- app/src/main/java/com/project/appealic/data/model/UserPlaylist.kt
+++ app/src/main/java/com/project/appealic/data/model/UserPlaylist.kt
@@ -1,8 +1,32 @@
 package com.project.appealic.data.model
 
-class UserPlaylist(
-    val playlistImage: Int,
-    val userName: String,
-    val playlistName: String
-) {
+class UserPlaylist  {
+    var name: String? = null
+    var image: String? = null
+    var uid: String? = null
+    var dateCreated: String? = null
+
+    constructor() : super()
+    constructor(id: String?, name: String?, image: String?, uid: String?, dateCreated: String?) : super() {
+        this.name = name
+        this.image = image
+        this.uid = uid
+        this.dateCreated = dateCreated
+    }
+
+    fun update(name: String?, image: String?, uid: String?, dateCreated: String?) {
+        this.name = name
+        this.image = image
+        this.uid = uid
+        this.dateCreated = dateCreated
+    }
+
+    override fun toString(): String {
+        return "UserPlaylist{" +
+                ", dateCreated='" + dateCreated + '\'' +
+                ", name='" + name + '\'' +
+                ", uid='" + uid + '\'' +
+                ", image='" + image + '\'' +
+                '}'
+    }
 }
