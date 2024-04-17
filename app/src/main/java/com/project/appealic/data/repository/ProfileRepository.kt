2b73fe60b9a6906f2fd879a3212b40b392b2d1e1
NQD190523 <duc19052003@gmail.com>
2024-04-17 16:23:53 +0700

    save working on profile

--- app/src/main/java/com/project/appealic/data/repository/ProfileRepository.kt
+++ app/src/main/java/com/project/appealic/data/repository/ProfileRepository.kt
@@ -1,13 +1,25 @@
 package com.project.appealic.data.repository
 
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.database.ktx.database
+import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.ktx.Firebase
 
 class ProfileRepository {
     private val user = FirebaseAuth.getInstance().currentUser
-    val firebaseDatabase = Firebase.database
+    val db = FirebaseFirestore.getInstance()
 
+    fun addUserToFirestore(user: FirebaseUser) {
+        val userData = hashMapOf(
+            "name" to "Nguyen Van A",
+            "age" to 30,
+            "email" to user.email
+        )
+        db.collection("users")
+            .document(user.uid)
+            .set(userData)
 
+    }
 
 }
