4d5e311524f97b30c9401891a456c45de2b2825c
NQD190523 <duc19052003@gmail.com>
2024-04-17 14:34:58 +0700

    save working on profile

---
+++ app/src/main/java/com/project/appealic/data/repository/ProfileRepository.kt
@@ -0,0 +1,8 @@
+package com.project.appealic.data.repository
+
+import com.google.firebase.auth.FirebaseAuth
+
+class ProfileRepository {
+    private val auth : FirebaseAuth = FirebaseAuth.getInstance()
+
+}
