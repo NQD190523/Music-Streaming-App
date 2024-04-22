a75612939e6f2f1c44da7b5d3e07495f3e255235
Lê Thị Băng Duyên <duyenltb21411@st.uel.edu.vn>
2024-04-22 22:22:33 +0700

    add confirm student

--- app/src/main/java/com/project/appealic/ui/view/Fragment/StudentPaymentFragment.kt
+++ app/src/main/java/com/project/appealic/ui/view/Fragment/StudentPaymentFragment.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageView
 import androidx.fragment.app.Fragment
 import com.project.appealic.R
 
@@ -18,4 +20,39 @@ class StudentPaymentFragment : Fragment() {
 
         return view
     }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val btnPaymentMomo = view.findViewById<Button>(R.id.btnPaymentMomo)
+        val btnPaymentCard = view.findViewById<Button>(R.id.btnPaymentCard)
+
+        // Thiết lập sự kiện click cho các nút
+        btnPaymentMomo.setOnClickListener {
+            navigateToConfirmStudentFragment()
+        }
+
+        btnPaymentCard.setOnClickListener {
+            navigateToConfirmStudentFragment()
+        }
+    }
+
+    private fun navigateToConfirmStudentFragment() {
+
+        // Khởi tạo một FragmentTransaction
+        val transaction = activity?.supportFragmentManager?.beginTransaction()
+        transaction?.replace(R.id.fragmenthome, ConfirmStudentFragment())
+        transaction?.addToBackStack(null)
+        transaction?.commit()
+    }
+
+    class ConfirmStudentFragment : Fragment() {
+        override fun onCreateView(
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
+        ): View? {
+            // Inflate the layout for this fragment
+            return inflater.inflate(R.layout.fragment_confirm_student, container, false)
+
+    }
+}
 }
